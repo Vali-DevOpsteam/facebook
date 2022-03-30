dcc1a7c54c02b157096a46c8b12d14852d9d0088
vali04091988 <valisheik78@gmail.com>
2022-03-30 20:37:27 +0530

    updated Deploy.sh file using master branch and committin

--- Deploy.sh
+++ Deploy.sh
@@ -5,4 +5,8 @@ again updted this file.
 
 updating this line
 
--- we are now again modifying this file using stage branch 
+-- we are now again modifying this file using stage branch
+
+updating this file using master branch
+
+** Keeping above messages aside, i am now updating this file using master branch to check git Merge **
